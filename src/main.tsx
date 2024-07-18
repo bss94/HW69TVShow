6a73f60eba085d43db811211c63d71de18393ae5
Sergey Brazhnikov <serjak.2012@gmail.com>
2024-07-19 02:43:10 +0600

    install react-router-dom react-bootstrap bootstrap @reduxjs/toolkit react-redux axios

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>,
+    </Provider>,
   </React.StrictMode>,
 )
